b844901a4214f1b2946e2bf2f3d08e5dcddeae0b
J. Ryan Stinnett <jryans@gmail.com>
2022-10-15 22:51:00 +0100

    Fix word separation in run script

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -2,12 +2,12 @@ llvm() {
   local build=$1
   local program=$2
   shift 2
-  $HOME/Projects/LLVM/llvm/build-$build/bin/$program $*
+  $HOME/Projects/LLVM/llvm/build-$build/bin/$program "$@"
 }
 
 klee() {
   local build=$1
   local program=$2
   shift 2
-  $HOME/Projects/klee/build-$build/bin/$program $*
+  $HOME/Projects/klee/build-$build/bin/$program "$@"
 }
