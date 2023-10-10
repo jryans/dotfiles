db4e5be3439d5d3d7e824de97b1800e124effabc
J. Ryan Stinnett <jryans@gmail.com>
2023-10-10 18:36:54 +0100

    Adjust path to LLVM builds

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -2,7 +2,7 @@ llvm() {
   local build=$1
   local program=$2
   shift 2
-  $HOME/Projects/LLVM/llvm/build-$build/bin/$program "$@"
+  $HOME/Projects/LLVM/llvm/builds/$build/bin/$program "$@"
 }
 
 klee() {
