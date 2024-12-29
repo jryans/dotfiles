13fd3ec8d7d0961e2a10ed1a52ec5877cb0d8692
J. Ryan Stinnett <jryans@gmail.com>
2024-12-29 01:45:37 +0000

    Add model-specific settings

--- .root/etc/nixos/flake.nix
+++ .root/etc/nixos/flake.nix
@@ -3,12 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixosHardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, nixosHardware, ... }: {
     nixosConfigurations.saturn = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
+        nixosHardware.nixosModules.framework-13-7040-amd
         ./configuration.nix
       ];
     };
