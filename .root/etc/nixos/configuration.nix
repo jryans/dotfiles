f4459de1975d6c7078e6d23f79d92ac66420b611
J. Ryan Stinnett <jryans@gmail.com>
2025-01-02 16:11:49 +0000

    Change locale to en_GB

--- .root/etc/nixos/configuration.nix
+++ .root/etc/nixos/configuration.nix
@@ -50,7 +50,7 @@
 
   time.timeZone = "Europe/London";
 
-  # i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "en_GB.UTF-8";
   # console = {
   #   font = "Lat2-Terminus16";
   #   keyMap = "us";
