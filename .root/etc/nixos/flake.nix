a7aa46c20bdcc4df56386ff587af5fc85d0ceb6b
J. Ryan Stinnett <jryans@gmail.com>
2025-04-09 20:49:54 +0100

    Wire up local nixpkgs for future use

--- .root/etc/nixos/flake.nix
+++ .root/etc/nixos/flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixosHardware.url = "github:NixOS/nixos-hardware/master";
+    nixpkgsLocal.url = "/home/jryans/Projects/Nix/nixpkgs";
   };
 
   outputs =
@@ -9,11 +10,22 @@
       self,
       nixpkgs,
       nixosHardware,
+      nixpkgsLocal,
       ...
     }:
     {
-      nixosConfigurations.saturn = nixpkgs.lib.nixosSystem {
+      nixosConfigurations.saturn = nixpkgs.lib.nixosSystem rec {
         system = "x86_64-linux";
+        specialArgs = {
+          pkgsLocal = import nixpkgsLocal {
+            inherit system;
+            config.allowUnfreePredicate =
+              pkg:
+              builtins.elem (nixpkgs.lib.getName pkg) [
+                "enpass"
+              ];
+          };
+        };
         modules = [
           nixosHardware.nixosModules.framework-13-7040-amd
           ./configuration.nix
