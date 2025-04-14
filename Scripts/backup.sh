14ccf5f2e69e23667913eec8095d276e5e8e6fe7
J. Ryan Stinnett <jryans@gmail.com>
2025-04-14 11:37:19 +0100

    Use HOME variable

--- Scripts/backup.sh
+++ Scripts/backup.sh
@@ -1,3 +1,3 @@
 export GOMAXPROCS=4
 
-./restic.sh backup -x --exclude-caches --exclude-file ./restic-exclude /home/jryans
+./restic.sh backup -x --exclude-caches --exclude-file ./restic-exclude ${HOME}
