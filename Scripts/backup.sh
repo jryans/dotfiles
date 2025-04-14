6a43b3c04f97aa5125d41535897b8f036433dadf
J. Ryan Stinnett <jryans@gmail.com>
2025-04-14 11:41:28 +0100

    Reformat backup script

--- Scripts/backup.sh
+++ Scripts/backup.sh
@@ -2,4 +2,9 @@ SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 
 export GOMAXPROCS=4
 
-"${SCRIPT_DIR}/restic.sh" backup -x --exclude-caches --exclude-file "${SCRIPT_DIR}/restic-exclude" ${HOME}
+"${SCRIPT_DIR}/restic.sh" \
+  backup \
+  -x \
+  --exclude-caches \
+  --exclude-file "${SCRIPT_DIR}/restic-exclude" \
+  ${HOME}
