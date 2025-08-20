fab99af70b1501e7e503e206ab6b14610fe736c7
J. Ryan Stinnett <jryans@gmail.com>
2025-08-20 19:59:42 +0100

    Add Haskell Nix flake

---
+++ .root/etc/nixos/haskell/flake.nix
@@ -0,0 +1,47 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      systems,
+      ...
+    }:
+    let
+      forEachSystem =
+        f:
+        nixpkgs.lib.genAttrs (import systems) (
+          system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+            };
+          }
+        );
+    in
+    {
+      devShells = forEachSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              alex
+              autoconf
+              automake
+              cabal-install
+              gmp
+              happy
+              haskell.compiler.ghc912
+              ncurses
+              python3
+              python3Packages.sphinx
+            ];
+            hardeningDisable = [ "fortify" ];
+          };
+        }
+      );
+    };
+}
